cb8948cb3bc8e0d10ddbb4040d3980c147d98c95
Mo Gharari <gharary@me.com>
2024-01-07 15:52:01 +0100

    Adding MigrationPlan to let SwiftData migrate between different schema versions

--- Muscle Tracker/DataModels/MigrationPlan.swift
+++ Muscle Tracker/DataModels/MigrationPlan.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import SwiftData
+
+enum WorkoutTypeMigrationPlan: SchemaMigrationPlan {
+    static var schemas: [VersionedSchema.Type] {
+        [WorkoutSchemaV1.self, WorkoutSchemaV2.self]
+    }
+    
+    static var stages: [MigrationStage] {
+        [migrateV1toV2]
+    }
+    
+    static let migrateV1toV2 = MigrationStage.lightweight(fromVersion: WorkoutSchemaV1.self, toVersion: WorkoutSchemaV2.self)
+}
